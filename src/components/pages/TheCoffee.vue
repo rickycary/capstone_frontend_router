fc6a32f3972dda40512ff5125e2b813b6f281311
Richard <ricky.cary1@gmail.com>
2023-03-26 14:21:19 -0500

    Index page functioning

--- src/components/pages/TheCoffee.vue
+++ src/components/pages/TheCoffee.vue
@@ -1,7 +1,43 @@
 <template>
     <h1>The Coffee Page</h1>
+    <router-link to="/new">Add a Coffee</router-link>
+    <div v-if="coffees.length">
+        <ul>
+          <li v-for="coffee in coffees" :key="coffee.id">
+            <router-link :to="`/coffee/${coffee.id}`"><h1>{{ coffee.name }}</h1></router-link>
+            <h2>{{ coffee.addOns }}</h2>
+            <h2>{{ coffee.location }}</h2>
+            </li>
+        </ul>
+      </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      coffees: []
+    }
+  },
+  mounted() {
+    fetch('https://coffee-backend.onrender.com/coffee')
+      .then(response => response.json())
+      .then(data => {
+        this.coffees = data.map(coffee => ({
+          id: coffee._id,
+          name: coffee.name,
+          addOns: coffee.addOns,
+          location: coffee.location,
+        })
+        );
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+}
 </script>
+
+<style scoped>
+
+</style>
--- src/components/pages/TheShow.vue
+++ src/components/pages/TheShow.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <h1>The Show Page</h1>
+        <router-link to="/new">Edit this Coffee</router-link>
+
     </div>
 </template>
 
