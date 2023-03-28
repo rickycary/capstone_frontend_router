9297231259ad5de4181124c963b0f84cb4220866
Richard <ricky.cary1@gmail.com>
2023-03-27 21:05:47 -0500

    Create page and functions work

--- src/components/pages/TheCreate.vue
+++ src/components/pages/TheCreate.vue
@@ -1,23 +1,79 @@
 <template>
-    <div>
-        <h1>Add a Coffee</h1>
-        <form @submit.prevent="addCoffee">
-            <!-- <label for="name">Name:</label> -->
-            <input type="text" name="name" placeholder="Name of Coffee" v-model="coffeeName">
-            <br>
-            <!-- <label for="addOns">Add Ons: </label> -->
-            <input type="text" name="addOns" placeholder="Add Ons" v-model="coffeeAddOns">
-            <br>
-            <!-- <label for="location">Location: </label> -->
-            <input type="text" name="location" placeholder="Coffee Shop Name" v-model="coffeeLocation">
-            <br>
-            <button type="submit">Submit</button>
-        </form>
+    <div class="main">
+      <h1>Add a New Coffee</h1>
+      <form @submit.prevent="createCoffee">
+          <label for="name">Name:</label>
+          <input 
+            type="text" 
+            name="name" 
+            v-model="coffeeName"><br/>
+
+          <label for="addOns">Add Ons:</label>
+          <input 
+            type="text" 
+            name="addOns" 
+            v-model="coffeeAddOns"><br/>
+
+          <label for="location">Location:</label>
+          <input 
+            type="text" 
+            name="location" 
+            v-model="coffeeLocation"><br/>
+
+        <button type="submit">Create</button>
+      </form>
     </div>
 </template>
-
+  
 <script>
-    export default {
-        
+  export default {
+  data() {
+    return {
+      coffeeName: '', coffeeAddOns: '', coffeeLocation: '', coffees: []
     }
+  },
+  methods: {
+    createCoffee() {
+      fetch('https://coffee-backend.onrender.com/coffee', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.coffeeName,
+          addOns: this.coffeeAddOns,
+          location: this.coffeeLocation,
+        })
+      })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data
+        console.log(data);
+        // Clear the form fields
+        this.coffeeName = '';
+        this.coffeeAddOns = '';
+        this.coffeeLocation = '';
+        
+        this.coffees.push(data);
+        // redirect to coffee
+        this.$router.push('/coffee/');
+      })
+      .then((response) => {
+          if (response.ok) {
+            // Takes you back to the main coffee page
+            this.$router.push(`/coffee/`);
+          } else {
+            throw new Error("Error creating coffee.");
+          }
+        })
+      .catch(error => {
+        console.error('Error creating new entry:', error);
+      });
+    }
+  }
+}
 </script>
+
+<style>
+
+</style>
