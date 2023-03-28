e511f136ce8eacbd82ea25ab8c0e66f3095294c1
Richard <ricky.cary1@gmail.com>
2023-03-27 20:34:19 -0500

    Created Form to add coffee

--- src/components/pages/TheCreate.vue
+++ src/components/pages/TheCreate.vue
@@ -1,7 +1,23 @@
 <template>
     <div>
         <h1>Add a Coffee</h1>
+        <form @submit.prevent="addCoffee">
+            <!-- <label for="name">Name:</label> -->
+            <input type="text" name="name" placeholder="Name of Coffee" v-model="coffeeName">
+            <br>
+            <!-- <label for="addOns">Add Ons: </label> -->
+            <input type="text" name="addOns" placeholder="Add Ons" v-model="coffeeAddOns">
+            <br>
+            <!-- <label for="location">Location: </label> -->
+            <input type="text" name="location" placeholder="Coffee Shop Name" v-model="coffeeLocation">
+            <br>
+            <button type="submit">Submit</button>
+        </form>
     </div>
 </template>
 
-<script></script>
+<script>
+    export default {
+        
+    }
+</script>
